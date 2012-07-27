f23b6519edd7567ec0797aa2a7ec1f84f43e4481
martin boissier <martin.boissier@gmail.com>
2012-07-27 18:34:53 +0100

    add sge script

---
+++ experiment/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$ -m be -M mrpb201@ex.ac.uk
+
+reduction[1]='PCA'
+reduction[2]='Isomap'
+reduction[3]='LLE'
+reduction[4]='Laplacian'
+
+cd dissertation/experiment
+
+octave --silent --eval "dimension_reduction ('view1.bin', $reduction[$SGE_TASK_ID], 300"
