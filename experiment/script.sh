bba8f36c0c3495c2512d9d9ef3edfdcb32ec586f
martin boissier <martin.boissier@gmail.com>
2012-07-27 18:36:30 +0100

    ok

--- experiment/script.sh
+++ experiment/script.sh
@@ -6,6 +6,6 @@ reduction[2]='Isomap'
 reduction[3]='LLE'
 reduction[4]='Laplacian'
 
-cd dissertation/experiment
+cd ~/dissertation/experiment
 
 octave --silent --eval "dimension_reduction ('view1.bin', $reduction[$SGE_TASK_ID], 300"
