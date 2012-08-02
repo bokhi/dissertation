b0dcfe263f524005c5b352c4e65b8af9d1e34248
martin boissier <martin.boissier@gmail.com>
2012-08-02 18:18:15 +0100

    art

---
+++ experiment/standardise.m
@@ -0,0 +1,12 @@
+function [] = standardise (file)
+  
+  load (file)
+
+  D = [Data; DataTT1; DataTT2];
+  D = zscore (D);
+
+  Data = D(1:size(Data, 1), :);
+  DataTT1 = D(size(Data, 1)+1:size(Data, 1)+size(DataTT1, 1), :);
+  DataTT2 = D(size(Data, 1)+size(DataTT1, 1):end, :);
+
+  save('-mat7-binary', ['s_' file], 'Data', 'DataTT1', 'DataTT2', 'SS', 'DD');  
