1a96a1f173c34a11b1c2e9694a3a52096c131171
martin boissier <martin.boissier@gmail.com>
2012-08-06 15:55:29 +0100

    num2string correction

--- experiment/sge_experiment.m
+++ experiment/sge_experiment.m
@@ -16,44 +16,44 @@ function [] = sge_experiment (n)
       %% file{1}
     case 1
       dimension_reduction (file{1}, reduction{1}, dimension{1});
-      accuracy ([reduction{1} '-' dimension{1} '_' file{1}]);
+      accuracy ([reduction{1} '-' num2str(dimension{1}) '_' file{1}]);
     case 2
       dimension_reduction (file{1}, reduction{2}, dimension{1});
-      accuracy ([reduction{2} '-' dimension{1} '_' file{1}]);
+      accuracy ([reduction{2} '-' num2str(dimension{1}) '_' file{1}]);
     case 3
       dimension_reduction (file{1}, reduction{3}, dimension{1});
-      accuracy ([reduction{3} '-' dimension{1} '_' file{1}]);
+      accuracy ([reduction{3} '-' num2str(dimension{1}) '_' file{1}]);
     case 4
       dimension_reduction (file{1}, reduction{4}, dimension{1});
-      accuracy ([reduction{4} '-' dimension{1} '_' file{1}]);
+      accuracy ([reduction{4} '-' num2str(dimension{1}) '_' file{1}]);
 
       %% file{2}
     case 5
       dimension_reduction (file{2}, reduction{1}, dimension{1});
-      accuracy ([reduction{1} '-' dimension{1} '_' file{2}]);
+      accuracy ([reduction{1} '-' num2str(dimension{1}) '_' file{2}]);
     case 6
       dimension_reduction (file{2}, reduction{2}, dimension{1});
-      accuracy ([reduction{2} '-' dimension{1} '_' file{2}]);
+      accuracy ([reduction{2} '-' num2str(dimension{1}) '_' file{2}]);
     case 7
       dimension_reduction (file{2}, reduction{3}, dimension{1});
-      accuracy ([reduction{3} '-' dimension{1} '_' file{2}]);
+      accuracy ([reduction{3} '-' num2str(dimension{1}) '_' file{2}]);
     case 8
       dimension_reduction (file{2}, reduction{4}, dimension{1});
-      accuracy ([reduction{4} '-' dimension{1} '_' file{2}]);
+      accuracy ([reduction{4} '-' num2str(dimension{1}) '_' file{2}]);
 
       %% file{3}
     case 9
       dimension_reduction (file{3}, reduction{1}, dimension{2});
-      accuracy ([reduction{1} '-' dimension{2} '_' file{3}]);
+      accuracy ([reduction{1} '-' num2str(dimension{2}) '_' file{3}]);
     case 10
       dimension_reduction (file{3}, reduction{2}, dimension{2});
-      accuracy ([reduction{2} '-' dimension{2} '_' file{3}]);
+      accuracy ([reduction{2} '-' num2str(dimension{2}) '_' file{3}]);
     case 11
       dimension_reduction (file{3}, reduction{3}, dimension{2});
-      accuracy ([reduction{3} '-' dimension{2} '_' file{3}]);
+      accuracy ([reduction{3} '-' num2str(dimension{2}) '_' file{3}]);
     case 12
       dimension_reduction (file{3}, reduction{4}, dimension{2});
-      accuracy ([reduction{4} '-' dimension{2} '_' file{3}]);
+      accuracy ([reduction{4} '-' num2str(dimension{2}) '_' file{3}]);
 
       %% M_LDA
     case 13
