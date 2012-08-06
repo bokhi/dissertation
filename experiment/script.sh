c2f60ff183a3d0fe545700883815c2b50f8c147b
martin boissier <martin.boissier@gmail.com>
2012-08-06 14:34:55 +0100

    modif script

--- experiment/script.sh
+++ experiment/script.sh
@@ -3,6 +3,6 @@
 
 cd ~/dissertation/experiment
 
-#octave --eval "sge_experiment ($SGE_TASK)"
-matlab -nodisplay -nodesktop -nosplash -nojvm -r "sge_experiment ($SGE_TASK)"
+#octave --eval "sge_experiment ($SGE_TASK_ID)"
+matlab -nodisplay -nodesktop -nosplash -nojvm -r "sge_experiment ($SGE_TASK_ID)"
 
