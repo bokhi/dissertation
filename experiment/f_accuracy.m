b0e47683ff02fcca25fa3aeab3e97fc8f5740c92
martin boissier <martin.boissier@gmail.com>
2012-08-13 21:58:33 +0100

    arst

--- experiment/f_accuracy.m
+++ experiment/f_accuracy.m
@@ -1,19 +1,15 @@
 function [] = f_accuracy (file, dim, nb_dim)
 
-  directory = '~/dissertation/';
-  database = [directory 'database/'];
-  result = [directory 'result/'];
-
-  load ([database file]);
+  load (file);
 
   [crtt, roctt] = accuracy (train_Data, test_Data, train_SS, train_DD, test_SS, test_DD, dim, nb_dim)
 
-  if (exist ([result 'accuracy_' file], 'file'))
+  if (exist (['accuracy_' file], 'file'))
     try 
-      load ([result 'accuracy_' file]);
+      load (['accuracy_' file]);
     catch err
       pause (rand () * 10);
-      load ([result 'accuracy_' file], CRTT, ROCTT);
+      load (['accuracy_' file], CRTT, ROCTT);
     end
   end
     
@@ -24,7 +20,7 @@ function [] = f_accuracy (file, dim, nb_dim)
   
   pid = feature('getpid')
   
-  save ([result pid '_accuracy_' file], 'CRTT', 'ROCTT');
+  save ([pid '_accuracy_' file], 'CRTT', 'ROCTT');
 
-  movefile([result pid '_accuracy_' file], [result 'accuracy_' file]);
+  movefile([pid '_accuracy_' file], ['accuracy_' file]);
 end
