cab1a89f74333fa8d43f22b15361b1e9ee765680
martin boissier <martin.boissier@gmail.com>
2012-08-13 23:47:40 +0100

    oien

--- experiment/f_accuracy.m
+++ experiment/f_accuracy.m
@@ -1,5 +1,7 @@
 function [] = f_accuracy (file, dim, nb_dim)
 
+  pid = feature('getpid')
+
   load (file);
 
   [crtt, roctt] = accuracy (train_Data, test_Data, train_SS, train_DD, test_SS, test_DD, dim, nb_dim);
@@ -13,12 +15,10 @@ function [] = f_accuracy (file, dim, nb_dim)
     end
   end
     
-  CRTT(dim:dim+nb_dim-1) = crtt;
+  CRTT(dim:dim+nb_dim-1) = crtt(1:nb_dim);
   for i = 1:nb_dim
     ROCTT{i+dim-1} = roctt{i};
   end
-  
-  pid = feature('getpid')
   
   save ([num2str(pid) '_accuracy_' file], 'CRTT', 'ROCTT');
 
