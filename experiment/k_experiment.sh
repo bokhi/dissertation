4429762b2d6ead814c43ab5de35d10dc8923de86
martin boissier <martin.boissier@gmail.com>
2012-08-15 00:18:31 +0100

    ien

--- experiment/k_experiment.sh
+++ experiment/k_experiment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 cd ~/dissertation/experiment
 
-qsub -N k_Isomap -t $1-$2 matlab-run.sh "k_experiment('Isomap', 150, $SGE_TASK_ID);quit"
-qsub -N k_LLE -t $1-$2 matlab-run.sh "k_experiment('LLE', 150, $SGE_TASK_ID);quit"
+qsub -N k_Isomap -t $1-$2 run-matlab.sh "k_experiment('Isomap', 150, $SGE_TASK_ID);quit"
+qsub -N k_LLE -t $1-$2 run-matlab.sh "k_experiment('LLE', 150, $SGE_TASK_ID);quit"
 
-qsub -N Isomap_PLOT -hold_jid k_Isomap matlab-run.sh "plot_k_experiment ('Isomap');quit"
-qsub -N LLE_PLOT -hold_jid k_LLE matlab-run.sh "plot_pcak_experiment ('LLE');quit"
+qsub -N Isomap_PLOT -hold_jid k_Isomap run-matlab.sh "plot_k_experiment ('Isomap');quit"
+qsub -N LLE_PLOT -hold_jid k_LLE run-matlab.sh "plot_pcak_experiment ('LLE');quit"
 
