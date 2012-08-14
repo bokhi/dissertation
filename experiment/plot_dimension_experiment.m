53022cbf422b26b195754ef8dbf4d11b2b5e932f
martin boissier <martin.boissier@gmail.com>
2012-08-14 17:13:36 +0100

    ien

--- experiment/plot_dimension_experiment.m
+++ experiment/plot_dimension_experiment.m
@@ -1,16 +1,17 @@
 function [] = plot_dimension_experiment ()
 
-  method{1} = 'PCA';
-  method{2} = 'LDA';
-  method{3} = 'Isomap';
-  method{4} = 'LLE';
-  method{5} = 'SIFT';
+  method{1} = 'SIFT';
+  method{2} = 'PCA';
+  method{3} = 'LDA';
+  method{4} = 'Isomap';
+%%  method{5} = 'LLE';
+
   
   figure;
   hold all;
   
   for i = 1:length (method)
-    if (i == 5)
+    if (i == 1)
       load ('accuracy_view1.mat');
     end
     load (['accuracy_' method{i} '_view1.mat']);
