33bf3df2c67dd2b27a98243ef2f853e4ea2f323a
martin boissier <martin.boissier@gmail.com>
2012-08-14 10:09:12 +0100

    ien

--- experiment/f_accuracy.m
+++ experiment/f_accuracy.m
@@ -1,27 +1,22 @@
 function [] = f_accuracy (file, dim, nb_dim)
 
-  pid = feature('getpid');
-
   load (file);
 
-  [crtt, roctt] = accuracy (train_Data, test_Data, train_SS, train_DD, test_SS, test_DD, dim, nb_dim);
+  [crtt] = accuracy (train_Data, test_Data, train_SS, train_DD, test_SS, test_DD, dim, nb_dim);
 
-  pause (rand () * 10);
-  if (exist (['accuracy_' file], 'file'))
-    try 
-      load (['accuracy_' file]);
-    catch err
-      pause (rand () * 10);
-      load (['accuracy_' file], CRTT, ROCTT);
-    end
-  end
-    
-  CRTT(dim:dim+nb_dim-1) = crtt(1:nb_dim);
-  for i = 1:nb_dim
-    ROCTT{i+dim-1} = roctt{i};
+  while (exist (['accuracy_' file '.lock'], 'file'))
+    pause (rand () * 10);
   end
   
-  save ([num2str(pid) '_accuracy_' file], 'CRTT', 'ROCTT');
+  fclose(fopen(['accuracy_' file '.lock'], 'w'))
 
-  movefile([num2str(pid) '_accuracy_' file], ['accuracy_' file]);
+  if (exist (['accuracy_' file], 'file'))
+    load (['accuracy_' file]);
+  end
+  
+  CRTT(dim:dim+nb_dim-1) = crtt(1:nb_dim);
+  
+  save (['accuracy_' file], 'CRTT');
+
+  delete (['accuracy_' file '.lock']);
 end
