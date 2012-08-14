db251a7d647e0e670fcff643f6aa6babad01c900
martin boissier <martin.boissier@gmail.com>
2012-08-14 17:43:36 +0100

    oi

--- experiment/dimension_experiment.sh
+++ experiment/dimension_experiment.sh
@@ -12,7 +12,9 @@ qsub -N ACC_PCA -hold_jid PCA -t 1:$DIM:50 -v FILE='PCA_view1.mat',NB_DIM=50 acc
 qsub -N ACC_LDA -hold_jid LDA -t 1:$DIM:50 -v FILE='LDA_view1.mat',NB_DIM=50 accuracy.sh
 qsub -N ACC_Isomap -hold_jid Isomap -t 1:$DIM:50 -v FILE='Isomap_view1.mat',NB_DIM=50 accuracy.sh
 qsub -N ACC_LLE -hold_jid LLE -t 1:$DIM:50 -v FILE='LLE_view1.mat',NB_DIM=50 accuracy.sh
-qsub -N ACC_SIFT -hold_jid LLE -t 1:$DIM:50 -v FILE='view1.mat',NB_DIM=50 accuracy.sh
+qsub -N ACC_SIFT -t 1:$DIM:50 -v FILE='view1.mat',NB_DIM=50 accuracy.sh
+
+qsub -N PLOT -hold_jid ACC_PCA,ACC_LDA,ACC_Isomap,ACC_LLE,ACC_SIFT run-matlab.sh "plot_dimension_experiment ();quit"
 
 
 
---
+++ experiment/run-matlab.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/dissertation/experiment
+
+matlab -nodisplay -nodesktop -nosplash -nojvm -r $1
