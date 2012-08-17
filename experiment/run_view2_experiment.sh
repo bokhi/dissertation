9c4ce8a087dcfd8f29592a5f115e224b920a773b
martin boissier <martin.boissier@gmail.com>
2012-08-17 13:42:24 +0100

    oien

--- experiment/run_view2_experiment.sh
+++ experiment/run_view2_experiment.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cd ~/dissertation/experiment
 
-qsub -N view2_LDA -t 1-10 -v METHOD=LDA view2_experiment.sh
-qsub -N view2_Isomap -t 1-10 -v METHOD=Isomap view2_experiment.sh
-qsub -N view2_LLE -t 1-10 -v METHOD=LLE view2_experiment.sh
+asub -N extract_fold run-matlab.sh "extract_fold();quit"
+
+qsub -N view2_LDA -hold_jid extract_fold -t 1-10 -v METHOD=LDA view2_experiment.sh
+qsub -N view2_Isomap -hold_jid extract_fold -t 1-10 -v METHOD=Isomap view2_experiment.sh
+qsub -N view2_LLE -hold_jid extract_fold -t 1-10 -v METHOD=LLE view2_experiment.sh
 
 qsub -N performance_LDA -hold_jid view2_LDA run-matlab.sh "view2_performance('LDA');quit"
 qsub -N performance_Isomap -hold_jid view2_Isomap run-matlab.sh "view2_performance('Isomap');quit"
