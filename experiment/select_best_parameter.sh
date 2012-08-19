8fbf88ecb05077c769ba858c6323fc284439bae9
martin boissier <martin.boissier@gmail.com>
2012-08-19 19:29:03 +0100

    oien

--- experiment/select_best_parameter.sh
+++ experiment/select_best_parameter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/dissertation/experiment
 
-matlab -nodisplay -nodesktop -nosplash -nojvm -r "select_best_parameter('$FOLD',$SGE_TASK_ID,'$METHOD',$K);quit"
+matlab -nodisplay -nodesktop -nosplash -nojvm -r "select_best_parameter($FOLD,$SGE_TASK_ID,'$METHOD',$K);quit"
 
 
 
