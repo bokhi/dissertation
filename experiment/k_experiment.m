20a3c6f91fd238ca12a61eb89d03e8373b3108b1
martin boissier <martin.boissier@gmail.com>
2012-08-21 11:37:34 +0100

    oien

--- experiment/k_experiment.m
+++ experiment/k_experiment.m
@@ -1,27 +1,27 @@
-function [] = k_experiment (method, no_dims, k)
+function [] = k_experiment (file, method, no_dims, k)
   %% estimate the influence of the k-neighbourhood parameters on the
   %% performances of non-linear dimension reduction methods
 
-  load ('view1');
+  load (file);
   
   [train_Data, test_Data] = dimension_reduction (train_Data, test_Data, train_SS, train_DD, method, no_dims, k);
   
   crtt = accuracy (train_Data, test_Data, train_SS, train_DD, test_SS, test_DD, 1, no_dims);
 
-  while (exist(['k_' method '.mat.lock'], 'file'))
+  while (exist(['k_' method '_' file '.mat.lock'], 'file'))
     pause (rand () * 10);
   end
 
-  fclose(fopen(['k_' method '.mat.lock'], 'w'));
+  fclose(fopen(['k_' method '_' file '.mat.lock'], 'w'));
 
-  if (exist (['k_' method '.mat'], 'file'))
+  if (exist (['k_' method '_' file '.mat'], 'file'))
     load (['k_' method '.mat']);
   end
 
   CRTT{k} = crtt;
 
-  save (['k_' method '.mat'], 'CRTT');
+  save (['k_' method '_' file '.mat'], 'CRTT');
 
-  delete (['k_' method '.mat.lock']);
+  delete (['k_' method '_' file '.mat.lock']);
 end
   
--- experiment/run_k_experiment.sh
+++ experiment/run_k_experiment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 cd ~/dissertation/experiment
 
-qsub -N k_Isomap -t $1-$2 -v METHOD=Isomap,DIM=150 k_experiment.sh 
-qsub -N k_LLE -t $1-$2 -v METHOD=LLE,DIM=150 k_experiment.sh 
+qsub -N k_Isomap -t $1-$2 -v FILE=view1,METHOD=Isomap,DIM=150 k_experiment.sh 
+qsub -N k_LLE -t $1-$2 -v FILE=view1,METHOD=LLE,DIM=150 k_experiment.sh 
+qsub -N k_Isomap_sqrt -t $1-$2 -v FILE=sqrt_view1,METHOD=Isomap,DIM=150 k_experiment.sh 
+qsub -N k_LLE_sqrt -t $1-$2 -v FILE=sqrt_view1,METHOD=LLE,DIM=150 k_experiment.sh 
 
-qsub -N Isomap_PLOT -hold_jid k_Isomap run-matlab.sh "plot_k_experiment('Isomap');quit"
-qsub -N LLE_PLOT -hold_jid k_LLE run-matlab.sh "plot_k_experiment('LLE');quit"
 
