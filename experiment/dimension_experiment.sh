fc27d3367744bbdc54f539069fd95b743a184264
martin boissier <martin.boissier@gmail.com>
2012-08-21 15:07:01 +0100

    oien

--- experiment/dimension_experiment.sh
+++ experiment/dimension_experiment.sh
@@ -13,17 +13,17 @@ qsub -N sqrt_LDA -v FILE=sqrt_view1,TECHNIQUE=LDA,DIMENSION=$DIM,PARAMETER=[] di
 qsub -N sqrt_Isomap -v FILE=sqrt_view1,TECHNIQUE=Isomap,DIMENSION=$DIM,PARAMETER=9 dimension_reduction.sh
 qsub -N sqrt_LLE -v FILE=sqrt_view1,TECHNIQUE=LLE,DIMENSION=$DIM,PARAMETER=8 dimension_reduction.sh
 
-qsub -N ACC_PCA -hold_jid PCA -t 1:$DIM:50 -v FILE='PCA_view1',NB_DIM=50 accuracy.sh
-qsub -N ACC_LDA -hold_jid LDA -t 1:$DIM:50 -v FILE='LDA_view1',NB_DIM=50 accuracy.sh
-qsub -N ACC_Isomap -hold_jid Isomap -t 1:$DIM:50 -v FILE='Isomap_view1',NB_DIM=50 accuracy.sh
-qsub -N ACC_LLE -hold_jid LLE -t 1:$DIM:50 -v FILE='LLE_view1',NB_DIM=50 accuracy.sh
-qsub -N ACC_SIFT -t 1:$DIM:50 -v FILE='view1',NB_DIM=50 accuracy.sh
+qsub -N ACC_PCA -hold_jid PCA -t 1-$DIM:50 -v FILE=PCA_view1,NB_DIM=50 accuracy.sh
+qsub -N ACC_LDA -hold_jid LDA -t 1-$DIM:50 -v FILE=LDA_view1,NB_DIM=50 accuracy.sh
+qsub -N ACC_Isomap -hold_jid Isomap -t 1-$DIM:50 -v FILE=Isomap_view1,NB_DIM=50 accuracy.sh
+qsub -N ACC_LLE -hold_jid LLE -t 1-$DIM:50 -v FILE=LLE_view1,NB_DIM=50 accuracy.sh
+qsub -N ACC_SIFT -t 1:$DIM:50 -v FILE=view1,NB_DIM=50 accuracy.sh
 
-qsub -N ACC_PCA -hold_jid sqrt_PCA -t 1:$DIM:50 -v FILE='PCA_sqrt_view1',NB_DIM=50 accuracy.sh
-qsub -N ACC_LDA -hold_jid sqrt_LDA -t 1:$DIM:50 -v FILE='LDA_sqrt_view1',NB_DIM=50 accuracy.sh
-qsub -N ACC_Isomap -hold_jid sqrt_Isomap -t 1:$DIM:50 -v FILE='Isomap_sqrt_view1',NB_DIM=50 accuracy.sh
-qsub -N ACC_LLE -hold_jid sqrt_LLE -t 1:$DIM:50 -v FILE='LLE_sqrt_view1',NB_DIM=50 accuracy.sh
-qsub -N ACC_SIFT -t 1:$DIM:50 -v FILE='sqrt_view1',NB_DIM=50 accuracy.sh
+qsub -N ACC_PCA -hold_jid sqrt_PCA -t 1-$DIM:50 -v FILE=PCA_sqrt_view1,NB_DIM=50 accuracy.sh
+qsub -N ACC_LDA -hold_jid sqrt_LDA -t 1-$DIM:50 -v FILE=LDA_sqrt_view1,NB_DIM=50 accuracy.sh
+qsub -N ACC_Isomap -hold_jid sqrt_Isomap -t 1-$DIM:50 -v FILE=Isomap_sqrt_view1,NB_DIM=50 accuracy.sh
+qsub -N ACC_LLE -hold_jid sqrt_LLE -t 1-$DIM:50 -v FILE=LLE_sqrt_view1,NB_DIM=50 accuracy.sh
+qsub -N ACC_SIFT -t 1-$DIM:50 -v FILE=sqrt_view1,NB_DIM=50 accuracy.sh
 
 
 
