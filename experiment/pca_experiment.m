77392ac8a9c4d2a20326727c43add1d09dfcf954
martin boissier <martin.boissier@gmail.com>
2012-08-22 17:11:10 +0100

    oien

--- experiment/pca_experiment.m
+++ experiment/pca_experiment.m
@@ -1,15 +1,19 @@
 function [] = pca_experiment (file, method, no_dims, k)
   %% perform a PCA pre-reduction before applying the given dimension reduction method
   %% computation can be distributed
+
+  pca_dims = [50 100]; %% PCA dimension to test
+  method_dims = [1 50];
+
   
   load (file);
 
   pca_train_Data = train_Data(:, 1:no_dims);
   pca_test_Data = test_Data(:, 1:no_dims);
     
-  [method_train_Data, method_test_Data] = dimension_reduction(pca_train_Data, pca_test_Data, train_SS, train_DD, method, no_dims, k);
+  [method_train_Data, method_test_Data] = dimension_reduction(pca_train_Data, pca_test_Data, train_SS, train_DD, method, method_dims(2), k);
 
-  crtt = accuracy (method_train_Data, method_test_Data, train_SS, train_DD, test_SS, test_DD, 1, no_dims);
+  crtt = accuracy (method_train_Data, method_test_Data, train_SS, train_DD, test_SS, test_DD, method_dims(1), method_dims(2));
 
   
   while (exist(['pca_' method '_' file '.lock'], 'file'))
