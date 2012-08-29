fb7095902232143f8c571b64bd3dae4153fc2599
martin boissier <martin.boissier@gmail.com>
2012-08-29 09:34:47 +0100

    oie

--- experiment/plot_k_experiment.m
+++ experiment/plot_k_experiment.m
@@ -13,7 +13,7 @@ function [] = plot_k_experiment (file, method)
     average(i) = mean (CRTT{i});
   end
   
-  plotyy(no_dims:length(CRTT), maximum(no_dims:length(CRTT)), no_dims:length(CRTT), CONN_COMP(no_dims:length(CRTT)), 'plot');
+  [AX,H1,H2] = plotyy(no_dims:length(CRTT), maximum(no_dims:length(CRTT)), no_dims:length(CRTT), CONN_COMP(no_dims:length(CRTT)), 'plot');
 
   set(get(AX(1),'Ylabel'),'String','best accuracy') 
   set(get(AX(2),'Ylabel'),'String','graph coverage') 
